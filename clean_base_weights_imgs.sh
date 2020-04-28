2c3298257acc78040fb9371859417a97a68f02f2
SamSamhuns <samhunsadamant@gmail.com>
2020-04-28 22:46:34 +0400

    Add cleaning scripts

---
+++ clean_base_weights_imgs.sh
@@ -0,0 +1,3 @@
+rm generated_imgs/*
+rm generator_trained_weights/*
+rmwq discriminator_trained_weights/*
---
+++ clean_improved_weights_imgs.sh
@@ -0,0 +1,3 @@
+rm improved_generated_imgs/*
+rm improved_generator_trained_weights/*
+rm improved_discriminator_trained_weights/*
